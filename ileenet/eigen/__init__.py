cfb32685aa7f8feac7642895a464355f38dd2a5e
Qun Yang <qyan327@aucklanduni.ac.nz>
2021-11-02 23:42:39 +1300

    Add inverse matrix iteration

---
+++ ileenet/eigen/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: Qun Yang
+@license: (C) Copyright 2021, University of Auckland
+@contact: qyan327@aucklanduni.ac.nz
+@date: 1/11/21 15:28
+@description:  
+@version: 1.0
+"""
