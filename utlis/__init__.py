de9699ad219d1192ba0c96d7ffc7b8bd22036567
Qun Yang <qyan327@aucklanduni.ac.nz>
2022-01-22 11:47:12 +1300

    Add utlis

---
+++ utlis/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: Qun Yang
+@license: (C) Copyright 2022, University of Auckland
+@contact: qyan327@aucklanduni.ac.nz
+@date: 14/01/22 20:34
+@description:  
+@version: 1.0
+"""
