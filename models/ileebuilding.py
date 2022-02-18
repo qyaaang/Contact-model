8224797f537efd5345d8f59ff4b66501ae8e5b8f
Qun Yang <qyan327@aucklanduni.ac.nz>
2022-02-19 10:46:31 +1300

    Update model for differentiable physical simulation

--- models/ileebuilding.py
+++ models/ileebuilding.py
@@ -78,19 +78,20 @@ class ILEEBuilding:
         model.node(9, self.params[1], self.args.h * 2)
         m_w = self.args.rou * self.args.h_w * self.args.b_w * self.args.h  # Lumped mass of wall
         m_c = self.args.rou * self.args.h_c * self.args.h_c * self.args.h  # Lumped mass of column
-        f_p = self.args.f_pi * (np.pi * self.args.d ** 2 / 4)  # PT force
+        f_p = self.args.f_pi * (np.pi * self.args.d ** 2 / 4) * self.args.num_bar  # PT force
         m_p = f_p / 9800  # Equivalent mass due to PT
         m_w_all = m_w + m_p / 2
-        m_c_all = m_c + self.args.mb / 2
-        model.mass(1, 0.5 * m_c_all, 0.5 * m_c_all, 0.5 * m_c_all)
-        model.mass(2, 1.0 * m_c_all, 1.0 * m_c_all, 1.0 * m_c_all)
-        model.mass(3, 0.5 * m_c_all, 0.5 * m_c_all, 0.5 * m_c_all)
+        m_c_all_1 = m_c + self.args.mb_1 / 2
+        m_c_all_2 = m_c + self.args.mb_2 / 2
+        model.mass(1, 0.5 * m_c_all_1, 0.5 * m_c_all_1, 0.5 * m_c_all_1)
+        model.mass(2, 0.5 * (m_c_all_1 + m_c_all_2), 0.5 * (m_c_all_1 + m_c_all_2), 0.5 * (m_c_all_1 + m_c_all_2))
+        model.mass(3, 0.5 * m_c_all_2, 0.5 * m_c_all_2, 0.5 * m_c_all_2)
         model.mass(4, 0.5 * m_w_all, 0.5 * m_w_all, 0.5 * m_w_all)
         model.mass(5, 1.0 * m_w_all, 1.0 * m_w_all, 1.0 * m_w_all)
         model.mass(6, 0.5 * m_w_all, 0.5 * m_w_all, 0.5 * m_w_all)
-        model.mass(7, 0.5 * m_c_all, 0.5 * m_c_all, 0.5 * m_c_all)
-        model.mass(8, 1.0 * m_c_all, 1.0 * m_c_all, 1.0 * m_c_all)
-        model.mass(9, 0.5 * m_c_all, 0.5 * m_c_all, 0.5 * m_c_all)
+        model.mass(7, 0.5 * m_c_all_1, 0.5 * m_c_all_1, 0.5 * m_c_all_1)
+        model.mass(8, 0.5 * (m_c_all_1 + m_c_all_2), 0.5 * (m_c_all_1 + m_c_all_2), 0.5 * (m_c_all_1 + m_c_all_2))
+        model.mass(9, 0.5 * m_c_all_2, 0.5 * m_c_all_2, 0.5 * m_c_all_2)
         a_w = self.args.h_w * self.args.b_w
         i_w = (self.args.b_w * self.args.h_w ** 3) / 12
         a_c = self.args.h_c * self.args.h_c
@@ -98,23 +99,17 @@ class ILEEBuilding:
         model.hertz_contact_2(1, self.params[0],
                               self.args.k_c1, self.params[2] * self.args.k_c1,
                               self.args.c_c1, self.params[3] * self.args.c_c1,
-                              self.args.mu, self.args.g_tol)
+                              self.params[4], self.args.g_tol)
         model.hertz_contact_2(2, self.params[1],
                               self.args.k_c1, self.params[2] * self.args.k_c1,
                               self.args.c_c1, self.params[3] * self.args.c_c1,
-                              self.args.mu, self.args.g_tol)
-        # model.beam_element(1, 1, 2, a_c, self.args.e, 0.6 * i_c)
-        # model.beam_element(2, 2, 3, a_c, self.args.e, 0.5 * i_c)
-        # model.beam_element(3, 4, 5, a_w, self.args.e, 0.2 * i_w)
-        # model.beam_element(4, 5, 6, a_w, self.args.e, 0.2 * i_w)
-        # model.beam_element(5, 7, 8, a_c, self.args.e, 0.6 * i_c)
-        # model.beam_element(6, 8, 9, a_c, self.args.e, 0.5 * i_c)
-        model.beam_element(1, 1, 2, a_c, self.args.e, self.params[4] * i_c)
-        model.beam_element(2, 2, 3, a_c, self.args.e, self.params[4] * i_c)
-        model.beam_element(3, 4, 5, a_w, self.args.e, self.params[5] * i_w)
-        model.beam_element(4, 5, 6, a_w, self.args.e, self.params[5] * i_w)
-        model.beam_element(5, 7, 8, a_c, self.args.e, self.params[4] * i_c)
-        model.beam_element(6, 8, 9, a_c, self.args.e, self.params[4] * i_c)
+                              self.params[4], self.args.g_tol)
+        model.beam_element(1, 1, 2, a_c, self.args.e, self.params[5] * i_c)
+        model.beam_element(2, 2, 3, a_c, self.args.e, self.params[5] * i_c)
+        model.beam_element(3, 4, 5, a_w, self.args.e, self.params[6] * i_w)
+        model.beam_element(4, 5, 6, a_w, self.args.e, self.params[6] * i_w)
+        model.beam_element(5, 7, 8, a_c, self.args.e, self.params[5] * i_c)
+        model.beam_element(6, 8, 9, a_c, self.args.e, self.params[5] * i_c)
         model.contact_element(7, 5, 2, 1)
         model.contact_element(8, 8, 5, 1)
         model.contact_element(9, 6, 3, 2)
